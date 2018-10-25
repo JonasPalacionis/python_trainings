fef539266625869951aef4c221c34ca6906156e0
Jonas Palacionis <jonas.palacionis@bazaarvoice.com>
2018-10-25 15:24:19 +0100

    Add files via upload

---
+++ practice_task.py
@@ -0,0 +1,115 @@
+# practice task_1
+
+# [ ] print your name
+print('Jonas')
+# [ ] print "is using Python"
+print('is using Python')
+
+# [ ] create a variable your_name and assign it a sting containing your name
+my_name = 'Jonas'
+#[ ] print your_name
+print(my_name)
+
+# [ ] create variables and assign values for: favorite_song, shoe_size, lucky_number
+favorite_song = 'Never had a friend like me'
+shoe_size = 43
+lucky_number = 4
+
+# [ ] print the value of each variable favorite_song, shoe_size, and lucky_number
+print(favorite_song)
+print(shoe_size)
+print(lucky_number)
+
+# [ ] print favorite_song with description
+print('My favorite song is ' + favorite_song + 'which is a song by 2pac')
+
+# [ ] print shoe_size with description
+print('Everyone thinks I am too short for my shoe size which is ' + str(shoe_size))
+
+# [ ] print lucky_number with description
+print('My lucky number is the best number, it is number ',lucky_number)
+
+# assign favorite_lucky_shoe using
+favorite_lucky_shoe = 'people may say ' + favorite_song + ' '+str(shoe_size) + ' times, but not ' + ' ' + str(lucky_number) + ' times'
+print(favorite_lucky_shoe)
+
+
+
+# [ ] display the type of 'your name' (use single quotes)
+type('jonas')
+
+# [ ] display the type of "save your notebook!" (use double quotes)
+type("save your notebook")
+
+# [ ] display the type of "25" (use quotes)
+type('25')
+
+# [ ] display the type of "save your notebook " + 'your name'
+type("save your notebook" + "jonas")
+
+# [ ] display the type of 25 (no quotes)
+type(25)
+
+
+# [ ] display the type of 25 + 10
+type(25+10)
+
+# [ ] display the type of 1.55
+type(1.55)
+
+# [ ] display the type of 1.55 + 25
+type(1.55 + 25)
+
+
+# assignments ***RUN THIS CELL*** before starting the section
+
+student_name = "Gus"
+student_age = 16
+student_grade = 3.5
+student_id = "ABC-000-000"
+
+# [ ] display the current type of the variable student_name
+type(student_name)
+
+# [ ] display the type of student_age
+type(student_age)
+
+# [ ] display the type of student_grade
+type(student_grade)
+
+# [ ] display the type of student_age + student_grade
+type(student_age + student_grade)
+
+# [ ] display the current type of student_id
+type(student_id)
+
+# assign new value to student_id
+student_id = 'a new value'
+
+# [ ] display the current of student_id
+student_id
+
+# [ ] create integer variables (x, y, z) and assign them 1-3 digit integers (no decimals - no quotes)
+x = 2
+y = 33
+z = 444
+
+# variable for sum
+xyz_sum = x + y + z
+print(xyz_sum)
+
+# [ ] fix the error / removed one double quote
+print("Hello World!")
+
+# [ ] fix the error / added qoutes
+print('strings have quotes and variables have names')
+
+# [ ] fix the error / quote or str()
+print( "I have $" + '5')
+print( "I have $" + str(5))
+
+# [ ] fix the error / same quotes
+print('always save the notebook')
+
+
+
---
+++ scratch.py
@@ -0,0 +1,155 @@
+# Module 1.1.1
+print('Hello World')
+
+# this is new cell
+print('after edit, save')
+
+# Module 1.1.2
+print(' string in single quotes')
+
+print(" string in double quotes ")
+
+# printing an Integer
+print(783)
+
+# printing a string made of Integer characters
+
+print('783')
+
+# assign a new string to the current_msg
+# change the value of current_msg and print it again
+current_msg = "Run this cell using Ctrl+Enter"
+print(current_msg)
+current_msg = 'new message for the variable'
+print(current_msg)
+
+# assigning a string value to a variable student_name
+student_name = 'Jonas'
+# printing the value of variable student_name
+print(student_name)
+# assigning the student_name variable  a different string value (a different name)
+student_name = 'Jonas Palacionis'
+# printing the value of variable student_name
+print(student_name)
+# assigning a 3rd different string value, to the variable name
+student_name = 'Jonas Palacionis 1994'
+# printing the value of variable name
+print(student_name)
+
+# assigning a value to a variable called bucket
+bucket = 'value one'
+# printing the value of bucket
+print(bucket)
+# assigning an Integer value (no quotes) to the variable bucket
+bucket = 123
+# printing the value of bucket
+print(bucket)
+
+# Module 1.1.3
+
+# showing the type after assigning bucket = a whole number value such as 16
+bucket = 123514
+type(bucket)
+
+# showing  the type after assigning bucket = a word in "double quotes"
+bucket = "a whole sentence"
+type(bucket)
+
+# displaying the type of 'single quoting' (use single quotes)
+bucket = 'same thing in single quotes'
+type(bucket)
+
+# displaying the type of "double quoting" (use double quotes)
+bucket = "'is this it'"
+type(bucket)
+
+# displaying the type of "12" (use quotes)
+bucket = '12'
+type(bucket)
+
+# displaying the type of 12 (no quotes)
+bucket = 12
+type(bucket)
+
+# displaying the type of -12 (no quotes)
+bucket = -12
+type(bucket)
+
+# displaying the type of 12.0 (no quotes)
+bucket = 12.0
+type(bucket)
+
+# displaying the type of 1.55
+bucket = 1.55
+type(bucket)
+
+# finding the type of the type(3) statement (no quotes) - just for fun
+type(type(3))
+
+# Module 1.1.4
+
+# adding 3 integer numbers
+2+3+4
+
+# adding a float number and an integer number
+1+1.5
+
+# Adding the string "This notebook belongs to " and a string with your first name
+"This notebook belongs to " + "Jonas"
+
+# Creating variables sm_number and big_number and assign numbers then add the numbers
+sm_number = 400
+big_number = 600
+sm_number + big_number
+
+# performing string addition in the variable named new_msg (add a string to "my favorite food is ")
+new_msg = "My favorite food is"
+print(new_msg + " lasagna")
+
+# performing Integer addition in the variable named new_msg (add 2 or more Integers)
+new_sum =   0
+print(new_sum + 1 + 2)
+
+# creating and print a new string variable, new_msg_2, that concatenates new_msg + a literal string
+new_msg_2 = 'I like learning python'
+print(new_msg_2 + ' a lot')
+
+# reviewing and run the code - then fix any Errors / removed double quotes
+total_cost = 3 + 45
+print(total_cost)
+
+# reviewing and run the code - then fix any Errors / added quotes to 123
+school_num = '123'
+print("the street number of Central School is " + school_num)
+
+# repairing the syntax error / matching quotes
+print('my socks do not match')
+
+# repairing the NameError  / print
+print("my socks match now")
+
+# repairing the syntax error / adding bracket
+print("Save the notebook frequently")
+
+# repairing the NameError / smallcaps
+student_name = "Alton"
+print(student_name)
+
+# repairing the TypeError / 3 -> "3"
+total = "3"
+print(total + " students are signed up for tutoring")
+
+# creating flying bird character art
+print("-             -")
+print("  \         /")
+print("    \ . . /")
+print("       V")
+
+# creating capital letter "E" character art
+print("++++++")
+print("+")
+print("+++")
+print("+")
+print("++++++")
+
+
